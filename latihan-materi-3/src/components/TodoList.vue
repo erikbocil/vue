59edc521946cb94e6e4301838c8189137b63e487
erikbocil <erikluis3006@gmail.com>
2023-07-28 18:52:32 +0700

    Tugas 3

---
+++ latihan-materi-3/src/components/TodoList.vue
@@ -0,0 +1,33 @@
+<script>
+export default {
+  name: 'TodoList',
+  data() {
+    return {
+      todo: '',
+      todos: [],
+    };
+  },
+  methods: {
+    addTodo: function () {
+      this.todos.push(this.todo);
+      this.todo = '';
+    },
+  },
+  computed: {
+    hebat: function () {
+      return this.todos.length > 3;
+    },
+  },
+};
+</script>
+
+<template>
+  <div>
+    <ol>
+      <li v-for="(todo, index) in todos" :key="index">{{ todo }}</li>
+    </ol>
+    <input type="text" v-model="todo" />
+    <button @click="addTodo">Tambahkan</button>
+    <p v-if="hebat">Hebat</p>
+  </div>
+</template>
